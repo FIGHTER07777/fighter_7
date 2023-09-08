6d7485f1f8c533f0a1bffca80dbea6929b0e3a99
ᖴเɢђͲᗴ尺  ⁷ <129475031+FIGHTER07777@users.noreply.github.com>
2023-09-08 16:02:22 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -22,17 +22,17 @@ global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
 global.owner = ['916909137213'] //ur owner number
-global.ownernomer = "916909137213" //ur owner number2
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
+global.ownernomer = "91690137213" //ur owner number2
+global.ownername = "fighter-7" //ur owner name
+global.ytname = "YT:Ms" //ur yt chanel name
 global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.location = "India, Westbengal, Kolkata" //ur location
 
 //new
-global.botname = "Cheems Bot MD V8"
-global.ownernumber = '916909137213'
-global.ownername = '🦄드림 가이 Xeon'
-global.ownerNumber = ["916909137213@s.whatsapp.net"]
+global.botname = "Fighter_7"
+global.ownernumber = '9093997532'
+global.ownername ='FIGHTER_07'
+global.ownerNumber = ["919093997532@s.whatsapp.net"]
 global.ownerweb = "https://youtube.com/@DGXeon"
 global.websitex = "https://youtu.be/L_SIk59QeAU"
 global.wagc = "https://chat.whatsapp.com/Dc2qyVeK8JbJq8Gr3U1pKH"
